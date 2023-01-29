fbf5071610025582cb166e84bbb99e5c336efd76
Wisarut Chaloemanukulwattana <wisarutkim012@gmail.com>
2023-01-29 14:41:24 +0700

    basic jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <SayHello />
+      <Form />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +24,46 @@ function App() {
       </header>
     </div>
   );
+}
+
+
+function SayHello(){
+  const name = "Conner"
+  const error = true
+  const test = "test"
+
+  return (
+    <>
+      <h1 style={{
+        textAlign: "center",
+        fontSize: '48px',
+        color: 'red',
+      }}>Hello <br /> {name.toLocaleUpperCase()}</h1>
+      <p>Hey</p>
+      {error ? <div>error</div> : <div>success</div>}
+
+      {test && <div>{test}</div>}
+
+      <h2>{error ? "success" : "false"}</h2>
+    </>
+  )
+}
+
+
+function Form() {
+  const props = {
+    id: 'input',
+    type: 'text',
+    name: 'text',
+    maxLength: 3
+  }
+
+  return (
+    <>
+      <label htmlFor="input">Input :</label>
+      <input {...props} placeholder="Enter" />
+    </>
+  )
 }
 
 export default App;
